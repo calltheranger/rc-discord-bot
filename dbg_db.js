454948bd8f74dcb6203695674ea84b24a8293f96
dan <dan@mesoform.com>
2026-02-20 01:49:41 +0100

    Fixes bugs

--- dbg_db.js
+++
@@ -1,6 +0,0 @@
-const Database = require('better-sqlite3');
-const db = new Database('record_club.db');
-const users = db.prepare("SELECT * FROM users WHERE record_club_username = 'callthetest'").all();
-console.log('Database state for callthetest:');
-console.log(JSON.stringify(users, null, 2));
-db.close();
