bffc47d5ef6d94ec4b9f6f929d02a02b6d701c12
dan <dan@mesoform.com>
2026-06-22 23:46:48 +0200

    Added retries

--- DiscordSummaryBot/src/summarizer.d.ts
+++
@@ -1,9 +0,0 @@
-import { Client } from 'discord.js';
-export interface ChannelHistory {
-    channelName: string;
-    messages: string[];
-}
-export declare const getDailyHistory: (client: Client) => Promise<ChannelHistory[]>;
-export declare const summarizeDailyHistory: (history: ChannelHistory[]) => Promise<string>;
-export declare const executeDailySummary: (client: Client, overrideChannelId?: string) => Promise<void>;
-//# sourceMappingURL=summarizer.d.ts.map
